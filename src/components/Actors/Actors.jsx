3bfe77d191dcf1476a66241823a5b765181b956e
ak24126315 <ak241263@gmail.com>
2023-08-05 12:17:29 +0530

    file and folder structure

---
+++ src/components/Actors/Actors.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Actors = () => {
+  return (
+    <div>Actors</div>
+  );
+};
+export default Actors;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
 
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+
+import useStyles from './styles';
+
+import {
+  Actors, MovieInformation, Profile, NavBar, Movies,
+} from '.';
+
 function App() {
+  const classes = useStyles();
   return (
-    <div>App1</div>
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.main}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route exact path="/movie/:id" element={<MovieInformation />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
